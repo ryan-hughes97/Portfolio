43d69b408c8792dc700b62e0b6f5ce04c8bd202e
ryan-hughes97 <ryan.hughes.jp@gmail.com>
2020-07-01 14:54:35 -0700

    Form validation

--- app.js
+++ app.js
@@ -41,8 +41,7 @@ function validateMessage() {
 }
 
 // Event Listeners
-form.addEventListener('submit', function() {
-
+submit.addEventListener('click', function() {
   validateName(fullName);
   validateEmail(email);
   validateMessage(message);
