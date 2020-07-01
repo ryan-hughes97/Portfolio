495886a98b05c72179c934abb02a053a5469fa65
ryan-hughes97 <ryan.hughes.jp@gmail.com>
2020-07-01 13:38:08 -0700

    Added form validation

---
+++ app.js
@@ -0,0 +1,53 @@
+// Form Submission
+const form = document.getElementById('form');
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const submit = document.getElementById('submit');
+
+const nameInvalid = document.getElementById('name-invalid');
+const emailInvalid = document.getElementById('email-invalid');
+const messageInvalid = document.getElementById('message-invalid');
+
+fullName.addEventListener('blur', validateName);
+email.addEventListener('blur', validateEmail);
+message.addEventListener('blur', validateMessage);
+
+// submit.addEventListener('click', checkInput);
+
+function validateName() {
+  const re = /^[A-Za-z]{2,20}$/;
+  if(!re.test(fullName.value)) {
+    nameInvalid.style.display = 'block';
+  } else {
+    nameInvalid.style.display = 'none';
+  }
+}
+
+function validateEmail() {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if(!re.test(email.value)) {
+    emailInvalid.style.display = 'block';
+  } else {
+    emailInvalid.style.display = 'none';
+  }
+}
+
+function validateMessage() {
+  if(message.value === '') {
+    messageInvalid.style.display = 'block';
+  } else {
+    messageInvalid.style.display = 'none';
+  }
+}
+
+
+// Event Listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  validateName(fullName);
+  validateEmail(email);
+  validateMessage(message);
+});
